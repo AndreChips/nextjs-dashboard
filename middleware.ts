4e4350493eea3ea7322beb91a8632d045cf51da2
Andre Chandra Putra <andrechandra4817@gmail.com>
2024-09-11 17:05:22 +0700

    Fixed Login Function

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import NextAuth from 'next-auth'
 import { authConfig } from './auth.config'
 
 export default NextAuth(authConfig).auth
+
 export const config = {
-  matcher: ['/((?!api|_next/static|_nect/image|.*||.png$).*)']
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)']
 }
